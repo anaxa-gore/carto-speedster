cac91e59de2b8f30e3f26ff4ed75ba75793ee1df
anaxa-gore <thomas.bonavia@hotmail.fr>
2016-09-01 11:11:15 +0200

    Multi layers merged into single one (not yet multi-threaded)

--- src/main/java/org/axana/cartospeedster/SpeedsterResource.java
+++ src/main/java/org/axana/cartospeedster/SpeedsterResource.java
@@ -3,9 +3,11 @@ package org.axana.cartospeedster;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by tbonavia on 30/08/2016.
@@ -15,6 +17,7 @@ public class SpeedsterResource {
     private HttpServletRequest request;
 
     @GET
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public void getInfo(@Suspended final AsyncResponse asyncResponse){
         // The request is submitted for further resolution
         SpeedsterManager.getInstance().submitRequest(this.request, asyncResponse);
